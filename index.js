d67812e03a841e9209ff236f479b230b1c1180dc
Mariusz <mariusz.lepkowski23@gmail.com>
2024-02-21 16:45:31 +0100

    Add event listeners to all drum buttons

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-document.querySelector("button").addEventListener("click", function() {
-    alert("I got clicked");
-});
+var drumButtons = document.querySelectorAll(".drum")
+
+for (button of drumButtons) {
+    button.addEventListener("click", function() {
+        alert("I got clicked");
+    });
+}
