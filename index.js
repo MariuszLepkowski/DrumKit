43d6d1c898fe32954f4f8125e4cd67ce4eac6c9b
Mariusz <mariusz.lepkowski23@gmail.com>
2024-02-21 15:50:21 +0100

    Add an anonymous function as EventListener (alternative way to define a function)

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-document.querySelector("button").addEventListener("click", handleClick);
-
-
-function handleClick() {
-    alert("I got clicked!");
-}
+document.querySelector("button").addEventListener("click", function() {
+    alert("I got clicked");
+});
