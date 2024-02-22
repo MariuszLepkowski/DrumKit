829b0acc5c3c595a29a922c8732ff0d8c0f86a25
Mariusz <mariusz.lepkowski23@gmail.com>
2024-02-22 13:42:29 +0100

    Add backround images to buttons

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var drumButtons = document.querySelectorAll(".drum")
 
 for (button of drumButtons) {
     button.addEventListener("click", function() {
-        var audio = new Audio('')
+
+        var audio = new Audio('sounds/crash.mp3');
+        audio.play();
+        
     });
 }
